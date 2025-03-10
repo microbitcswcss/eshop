58d4b7fcc73a4dd117b252bfafb0c08b7f611fb6
Hinson Chu <129246500+microbitcswcss@users.noreply.github.com>
2025-03-10 20:33:26 +0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,18 +3,18 @@
         "id": 1,
         "name": "商品1",
         "price": 100,
-        "image": "img/product1.jpg"
+        "image": "product1.jpg"
     },
     {
         "id": 2,
         "name": "商品2",
         "price": 150,
-        "image": "img/product2.jpg"
+        "image": "product2.jpg"
     },
     {
         "id": 3,
         "name": "商品3",
         "price": 200,
-        "image": "img/product3.jpg"
+        "image": "product3.jpg"
     }
 ]
