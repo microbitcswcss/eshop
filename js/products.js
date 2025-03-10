21a9db1af26514e5ed06ccba99853d60d89b393b
Hinson Chu <129246500+microbitcswcss@users.noreply.github.com>
2025-03-10 20:26:07 +0800

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,20 @@
+[
+    {
+        "id": 1,
+        "name": "商品1",
+        "price": 100,
+        "image": "img/product1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "商品2",
+        "price": 150,
+        "image": "img/product2.jpg"
+    },
+    {
+        "id": 3,
+        "name": "商品3",
+        "price": 200,
+        "image": "img/product3.jpg"
+    }
+]
